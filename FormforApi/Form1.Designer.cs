7e6e721ba9a639b0b4f5e9d132e5ce451f2dacae
Emir Aydemir <emirmaydemir@gmail.com>
2021-09-07 12:38:28 +0300

    Add project files.

---
+++ FormforApi/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace FormforApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ekle = new System.Windows.Forms.Button();
+            this.Sil = new System.Windows.Forms.Button();
+            this.Güncelle = new System.Windows.Forms.Button();
+            this.Ders = new System.Windows.Forms.TextBox();
+            this.Kod = new System.Windows.Forms.TextBox();
+            this.Saat = new System.Windows.Forms.TextBox();
+            this.Kredi = new System.Windows.Forms.TextBox();
+            this.Hoca = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(802, 255);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Ekle
+            // 
+            this.Ekle.Location = new System.Drawing.Point(202, 414);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(75, 23);
+            this.Ekle.TabIndex = 3;
+            this.Ekle.Text = "Ekle";
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.Ders_btn_Click);
+            // 
+            // Sil
+            // 
+            this.Sil.Location = new System.Drawing.Point(322, 414);
+            this.Sil.Name = "Sil";
+            this.Sil.Size = new System.Drawing.Size(75, 23);
+            this.Sil.TabIndex = 4;
+            this.Sil.Text = "Sil";
+            this.Sil.UseVisualStyleBackColor = true;
+            this.Sil.Click += new System.EventHandler(this.Sil_Click);
+            // 
+            // Güncelle
+            // 
+            this.Güncelle.Location = new System.Drawing.Point(438, 414);
+            this.Güncelle.Name = "Güncelle";
+            this.Güncelle.Size = new System.Drawing.Size(75, 23);
+            this.Güncelle.TabIndex = 5;
+            this.Güncelle.Text = "Güncelle";
+            this.Güncelle.UseVisualStyleBackColor = true;
+            this.Güncelle.Click += new System.EventHandler(this.Güncelle_Click);
+            // 
+            // Ders
+            // 
+            this.Ders.Location = new System.Drawing.Point(71, 475);
+            this.Ders.Name = "Ders";
+            this.Ders.Size = new System.Drawing.Size(100, 20);
+            this.Ders.TabIndex = 8;
+            // 
+            // Kod
+            // 
+            this.Kod.Location = new System.Drawing.Point(177, 475);
+            this.Kod.Name = "Kod";
+            this.Kod.Size = new System.Drawing.Size(100, 20);
+            this.Kod.TabIndex = 9;
+            // 
+            // Saat
+            // 
+            this.Saat.Location = new System.Drawing.Point(638, 344);
+            this.Saat.Name = "Saat";
+            this.Saat.Size = new System.Drawing.Size(100, 20);
+            this.Saat.TabIndex = 10;
+            // 
+            // Kredi
+            // 
+            this.Kredi.Location = new System.Drawing.Point(494, 344);
+            this.Kredi.Name = "Kredi";
+            this.Kredi.Size = new System.Drawing.Size(100, 20);
+            this.Kredi.TabIndex = 11;
+            // 
+            // Hoca
+            // 
+            this.Hoca.Location = new System.Drawing.Point(283, 475);
+            this.Hoca.Name = "Hoca";
+            this.Hoca.Size = new System.Drawing.Size(100, 20);
+            this.Hoca.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 344);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(177, 344);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(333, 344);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 489);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Hoca);
+            this.Controls.Add(this.Kredi);
+            this.Controls.Add(this.Saat);
+            this.Controls.Add(this.Kod);
+            this.Controls.Add(this.Ders);
+            this.Controls.Add(this.Güncelle);
+            this.Controls.Add(this.Sil);
+            this.Controls.Add(this.Ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Ekle;
+        private System.Windows.Forms.Button Sil;
+        private System.Windows.Forms.Button Güncelle;
+        private System.Windows.Forms.TextBox Ders;
+        private System.Windows.Forms.TextBox Kod;
+        private System.Windows.Forms.TextBox Saat;
+        private System.Windows.Forms.TextBox Kredi;
+        private System.Windows.Forms.TextBox Hoca;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FormforApi/Form1.cs
@@ -0,0 +1,371 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace FormforApi
+{
+    public partial class Form1 : Form
+    {
+        string result;
+        string result2;
+        string result3;
+        int a = 0;
+        int sayac1 = 0;
+        int sayac2 = -1;
+        string tablo_ders;
+        string tablo_hoca;
+        string tablo_kod;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           fonk();
+           fillcombo();
+        }
+        private void fonk()
+        {
+            /*using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8095/");
+                HttpResponseMessage res = await client.GetAsync("api/Default/Get");
+                result = await res.Content.ReadAsStringAsync();
+                dataGridView1.DataSource = JsonConvert.DeserializeObject<List<Stay>>(result);
+
+                //dataGridView1.DataSource = result.ToList();
+                //doldur();
+            }*/
+            var client = new RestClient("http://localhost:8095/");
+            var request = new RestRequest("api/Default/Get", Method.GET);
+            var queryResult = client.Execute<List<Stay>>(request).Data;
+            dataGridView1.DataSource = queryResult;
+        }
+
+        public async void fillcombo()
+        {
+            // using (var client = new HttpClient())
+            // {
+            //client.BaseAddress = new Uri("http://localhost:8095/");
+            // HttpResponseMessage res = await client.GetAsync("api/Default/GetDersAdi");
+            //result = await res.Content.ReadAsStringAsync();
+
+            var client = new RestClient("http://localhost:8095/");
+            var request = new RestRequest("api/Default/GetDersAdi", Method.GET);
+            var queryResult = client.Execute<List<Stay>>(request).Data;
+            //JsonConvert.DeserializeObject<List<Stay>>(result)
+
+            foreach (var item in queryResult)
+                {
+                    comboBox1.Items.Add(item.DersAdi);
+                }
+           // }
+        }
+
+        /* public void doldur()
+         {
+             listele();
+             int a = 0;
+             int sayac1 = 0;
+             int sayac2 = -1;
+             dataGridView1.Rows.Clear();
+             dataGridView1.Columns.Clear();
+             result = result.Replace("[", "");
+             result = result.Replace("]", "");
+             result = result.Replace("\"", "");
+             List<string> l = new List<string>();
+             l.Add("Dersler"); l.Add("Kod"); l.Add("Hoca"); l.Add("Kredi"); l.Add("Saat");
+             dataGridView1.Columns.Add("Dersler", "Dersler"); dataGridView1.Columns.Add("Kod", "Kod"); dataGridView1.Columns.Add("Hoca", "Hoca"); dataGridView1.Columns.Add("Kredi", "Kredi"); dataGridView1.Columns.Add("Saat", "Saat");
+
+             foreach (string item in result.Split(','))
+             {
+                 a++;
+                 if (a % 5 == 0)
+                 {
+                     dataGridView1.Rows.Add();
+                 }
+             }
+
+             foreach (string item in result.Split(','))
+             {
+                 sayac2++;
+                 dataGridView1.Rows[sayac1].Cells[l[sayac2]].Value = item;
+                 if (sayac2 == 4)
+                 {
+                     sayac2 = -1;
+                     sayac1++;
+                 }
+             }
+         }*/
+
+        /* public async void listele()
+         {
+             using (var client = new HttpClient())
+             {
+                 client.BaseAddress = new Uri("http://localhost:8095/");
+                 HttpResponseMessage res = await client.GetAsync("api/Default");
+                 result = await res.Content.ReadAsStringAsync();
+                 //dataGridView1.DataSource = result.ToList();
+             }
+         }
+        */
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ders_btn_Click(object sender, EventArgs e)
+        {
+            string ders = Convert.ToString(comboBox1.Text);
+            string dersKodu = Convert.ToString(comboBox2.Text);
+            string kredi = Convert.ToString(Kredi.Text);
+            string saat = Convert.ToString(Saat.Text);
+            string hoca = Convert.ToString(comboBox3.Text);
+
+            Stay s = new Stay();
+            s.DersAdi = ders;
+            s.DersKodu = dersKodu;
+            s.Kredi = kredi;
+            s.Saat = saat;
+            s.Hoca = hoca;
+
+            /*using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8095/");
+                // client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(
+                //new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //var res = client.PostAsync("api/Default", s).Result;,
+                var json = JsonConvert.SerializeObject(s);
+                var asd = new JavaScriptSerializer().Serialize(s);
+                var res = client.PostAsync("api/Default/Post", new StringContent(asd
+                     , Encoding.UTF8, "application/json")).Result;
+                string resultContent =  res.Content.ReadAsStringAsync().Result;
+                MessageBox.Show(resultContent);
+                //dataGridView1.DataSource = result.ToList();
+                //doldur();
+                fonk();
+            }*/
+
+            var client = new RestClient("http://localhost:8095/");
+            var request = new RestRequest("api/Default/Post", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Stay
+            {
+            DersAdi = ders,
+            DersKodu = dersKodu,
+            Kredi = kredi,
+            Saat = saat,
+            Hoca = hoca
+        });
+            client.Execute(request);
+            fonk();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            comboBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["DersAdi"].FormattedValue.ToString();
+            comboBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["DersKodu"].FormattedValue.ToString();
+            comboBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["Hoca"].FormattedValue.ToString();
+            Kredi.Text = dataGridView1.Rows[e.RowIndex].Cells["Kredi"].FormattedValue.ToString();
+            Saat.Text = dataGridView1.Rows[e.RowIndex].Cells["Saat"].FormattedValue.ToString();
+        }
+
+        private async void Sil_Click(object sender, EventArgs e)
+        {
+            string ders = Convert.ToString(comboBox1.Text);
+            string dersKodu = Convert.ToString(comboBox2.Text);
+            string kredi = Convert.ToString(Kredi.Text);
+            string saat = Convert.ToString(Saat.Text);
+            string hoca = Convert.ToString(comboBox3.Text);
+
+           /* Stay s2 = new Stay();
+            //s.Kredi = kredi;
+            //s.Saat = saat;
+            s2.Hoca = hoca;*/
+
+            
+
+           // using (var client = new HttpClient())
+           // {
+                /*client.BaseAddress = new Uri("http://localhost:8095/");
+                // client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(
+                //new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //var res = client.PostAsync("api/Default", s).Result;
+                //await client.DeleteAsync("api/Default" , s2);
+                //var res = client.DeleteAsync("api/Default", new StringContent(
+                //      new JavaScriptSerializer().Serialize(s2), Encoding.UTF8, "application/json")).Result;
+                // string resultContent = res.Content.ReadAsStringAsync().Result;
+                //MessageBox.Show(response.ToString());
+                //dataGridView1.DataSource = result.ToList();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = new Uri("http://localhost:8095/api/Default/Delete"),
+                    Content = new StringContent(JsonConvert.SerializeObject(s2), Encoding.UTF8, "application/json")
+                };
+                var response = await client.SendAsync(request);*/
+               
+                var c = new RestClient("http://localhost:8095/");
+                var request = new RestRequest("api/Default/Delete", Method.DELETE);
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(new Stay
+                {
+                    DersAdi = ders,
+                    DersKodu = dersKodu,
+                    Kredi = kredi,
+                    Saat = saat,
+                    Hoca = hoca
+                });
+                c.Execute(request);
+                fonk();
+
+            //}
+
+
+
+        }
+
+        private void Güncelle_Click(object sender, EventArgs e)
+        {
+            string ders = Convert.ToString(comboBox1.Text);
+            string dersKodu = Convert.ToString(comboBox2.Text);
+            string kredi = Convert.ToString(Kredi.Text);
+            string saat = Convert.ToString(Saat.Text);
+            string hoca = Convert.ToString(comboBox3.Text);
+            /*
+                        Stay s3 = new Stay();
+                        s3.DersAdi = ders;
+                        s3.DersKodu = dersKodu;
+                        s3.Kredi = kredi;
+                        s3.Saat = saat;
+                        s3.Hoca = hoca;
+                        using (var client = new HttpClient())
+                        {
+                            client.BaseAddress = new Uri("http://localhost:8095/");
+
+
+                            var res = client.PutAsync("api/Default/Put", new StringContent(
+                                     new JavaScriptSerializer().Serialize(s3), Encoding.UTF8, "application/json")).Result;
+                            string resultContent = res.Content.ReadAsStringAsync().Result;
+                            MessageBox.Show(resultContent);
+                            //dataGridView1.DataSource = result.ToList();
+                            fonk();
+                        }*/
+
+            var c = new RestClient("http://localhost:8095/");
+            var request = new RestRequest("api/Default/Put", Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Stay
+            {
+                DersAdi = ders,
+                DersKodu = dersKodu,
+                Kredi = kredi,
+                Saat = saat,
+                Hoca = hoca
+            });
+            c.Execute(request);
+            fonk();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            string bul = comboBox1.SelectedItem.ToString();
+            //Stay sa = new Stay();
+            //sa.DersAdi=bul;
+
+            // using (var client = new HttpClient())
+            //  {
+            //  client.BaseAddress = new Uri("http://localhost:8095/");
+            //  HttpResponseMessage res2 = await client.GetAsync("api/Default/GetDersKodu/"+bul);
+            //  result2 = await res2.Content.ReadAsStringAsync();
+            // dataGridView1.DataSource = JsonConvert.DeserializeObject<List<Stay>>(result);
+            //  HttpResponseMessage res3 = await client.GetAsync("api/Default/GetHoca/"+bul);
+            //  result3 = await res3.Content.ReadAsStringAsync();
+            //JsonConvert.DeserializeObject<List<Stay>>(result2);
+            // }
+
+            var client = new RestClient("http://localhost:8095/");
+            var request = new RestRequest("api/Default/GetDersKodu/"+bul, Method.GET);
+            var queryResult = client.Execute<List<Stay>>(request).Data;
+            var request2 = new RestRequest("api/Default/GetHoca/"+ bul, Method.GET);
+            var queryResult2 = client.Execute<List<Stay>>(request2).Data;
+
+            //JsonConvert.DeserializeObject<List<Stay>>(result2)
+            //JsonConvert.DeserializeObject<List<Stay>>(result3)
+
+            foreach (var item in queryResult)
+            {
+                comboBox2.Items.Add(item.DersKodu);
+            }
+
+           foreach (var item in queryResult2)
+            {
+                comboBox3.Items.Add(item.Hoca);
+            }
+        }
+
+        private async void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string aranan = textBox1.Text;
+
+            /*using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8095/");
+                HttpResponseMessage res = await client.GetAsync("api/Default/GetFind/"+aranan);
+                result = await res.Content.ReadAsStringAsync();
+                dataGridView1.DataSource = JsonConvert.DeserializeObject<List<Stay>>(result);
+
+                //dataGridView1.DataSource = result.ToList();
+                //doldur();
+            }*/
+
+            if (aranan == "")
+            {
+                fonk();
+            }
+            else
+            {
+                var client = new RestClient("http://localhost:8095/");
+                var request = new RestRequest("api/Default/GetFind/" + aranan, Method.GET);
+                var queryResult = client.Execute<List<Stay>>(request).Data;
+                dataGridView1.DataSource = queryResult;
+            }
+            
+        }
+    }
+
+    public class Stay
+    {
+        public string DersAdi { get; set; }
+        public string DersKodu { get; set; }
+        public string Kredi { get; set; }
+        public string Saat { get; set; }
+        public string Hoca { get; set; }
+
+    }
+}
